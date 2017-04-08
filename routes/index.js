12afd517569602f6c77c3c233b0f51afb45d9d07
Septian A Tama <sfirst.tama@gmail.com>
2017-04-08 22:15:56 +0700

    modifying index routes for sending json instead of rendering

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET main endpoint. */
+router.get('/', (req, res, next) => {
+  res.send({ message: 'Welcome Buddy!' });
 });
 
 module.exports = router;

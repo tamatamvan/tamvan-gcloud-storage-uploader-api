7c5187e2312de11f59046486486479a32d8c38a9
Septian A Tama <sfirst.tama@gmail.com>
2017-04-08 22:37:13 +0700

    deleting favicon on app js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict'
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');

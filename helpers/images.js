ea3da8ee6c2d65a49407a615adc106f7317589ee
Septian A Tama <sfirst.tama@gmail.com>
2017-07-07 10:28:17 +0700

    make send to gcstorage function

--- helpers/images.js
+++ helpers/images.js
@@ -14,6 +14,36 @@ const getPublicUrl = (filename) => {
   return `https://storage.googleapis.com/${CLOUD_BUCKET}/${filename}`;
 }
 
+const sendUploadToGCS = (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const gcsname = Date.now() + req.file.originalname;
+  const file = bucket.file(gcsname);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
+
+  stream.on('error', (err) => {
+    req.file.cloudStorageError = err;
+    next(err);
+  });
+
+  stream.on('finish', () => {
+    req.file.cloudStorageObject = gcsname;
+    file.makePublic().then(() => {
+      req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+      next();
+    });
+  });
+
+  stream.end(req.file.buffer);
+}
+
 const Multer = require('multer'),
       multer = Multer({
         storage: Multer.MemoryStorage,

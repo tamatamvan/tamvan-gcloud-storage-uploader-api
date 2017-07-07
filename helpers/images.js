c25052f87ea9728da25610198e99649e472083f9
Septian A Tama <sfirst.tama@gmail.com>
2017-07-07 10:17:11 +0700

    initiate images helper using multer

---
+++ helpers/images.js
@@ -0,0 +1,8 @@
+'use strict'
+const Multer = require('multer'),
+      multer = Multer({
+        storage: Multer.MemoryStorage,
+        limits: {
+          fileSize: 5 * 1024 * 1024
+        }
+      })

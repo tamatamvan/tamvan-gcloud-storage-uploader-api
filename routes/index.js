050ebe349bca64c2e05eb9a452f2eee4a80463a8
Septian A Tama <sfirst.tama@gmail.com>
2017-07-07 10:50:52 +0700

    add upload route and fixing type project name

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 'use strict'
 const express = require('express');
 const router = express.Router();
+const images = require('../helpers/images')
 
 /* GET main endpoint. */
 router.get('/', (req, res, next) => {
   res.send({ message: 'Welcome Buddy!' });
 });
+router.post('/upload', 
+  images.multer.single('image'), 
+  images.sendUploadToGCS,
+  (req, res) => {
+    res.send(req.cloudStoragePublicUrl)
+  })
 
 module.exports = router;

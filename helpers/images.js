4b41ebf9340624487458b07cb77ee010c8a158e9
Septian A Tama <sfirst.tama@gmail.com>
2017-07-07 20:07:11 +0700

    delete console.log

--- helpers/images.js
+++ helpers/images.js
@@ -16,8 +16,6 @@ const getPublicUrl = (filename) => {
 }
 
 const sendUploadToGCS = (req, res, next) => {
-  console.log('upload yuk')
-  console.log(req.file)
   if (!req.file) {
     return next()
   }
